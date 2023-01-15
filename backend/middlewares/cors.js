dfa6e99affcfaa41d251e46baaf6241b244be656
Andrey Morogin <andreandreandre10071998@yandex.ru>
2023-01-15 20:04:00 +0300

    test: cors

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,18 +1,9 @@
 const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-const allowedCors = [
-  'https://api.andreymorogin.mesto.nomoredomains.rocks',
-  'http://api.andreymorogin.mesto.nomoredomains.rocks',
-  'localhost:3000',
-];
-
 module.exports = (req, res, next) => {
   const { method } = req;
   const requestHeaders = req.headers['access-control-request-headers'];
-  const { origin } = req.headers;
 
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
+  res.header('Access-Control-Allow-Origin', '*');
 
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
